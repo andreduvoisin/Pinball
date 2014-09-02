bcc982ae49e81855e921c78980f5edec565f3141
Andre Duvoisin <duvoisin@usc.edu>
2014-09-02 03:55:09 -0700

    Left and right paddles fully functional.

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -3,18 +3,46 @@ using System.Collections;
 
 public class PaddleController : MonoBehaviour
 {
+	public KeyCode key;
+	private float initialVelocity;
+
 	// Use this for initialization
 	void Start()
 	{
-
+		initialVelocity = hingeJoint.motor.targetVelocity;
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		if(Input.GetKeyDown(KeyCode.Z))
+		CheckPaddleInput();
+	}
+
+	void CheckPaddleInput()
+	{
+		// initialVelocity is "down" for the paddle
+		if(Input.GetKeyDown(key))
 		{
-			hingeJoint.useMotor = true;
+			// if "down", change to "up"
+			if(hingeJoint.motor.targetVelocity == initialVelocity)
+			{
+				InvertMotorVelocity();
+			}
 		}
+		else if(Input.GetKeyUp(key))
+		{
+			// if "up", change to "down"
+			if(hingeJoint.motor.targetVelocity != initialVelocity)
+			{
+				InvertMotorVelocity();
+			}
+		}
+	}
+
+	void InvertMotorVelocity()
+	{
+		JointMotor motor = hingeJoint.motor;
+		motor.targetVelocity *= -1;
+		hingeJoint.motor = motor;
 	}
 }
